3a97dda29f2aa977639ede0f9b28a54279971b8a
DavidStreid <david.streid.nyc@gmail.com>
2020-04-10 18:39:49 -0400

    Adding work dir

---
+++ bam_creation.sh
@@ -0,0 +1,56 @@
+if [[ -z "$1"  ||  -z "$2" || -z "$3" ]]
+   then
+      echo "ERROR: Need to specify fastqs and samples"
+      exit 1
+fi
+
+F1=$1
+F2=$2
+SAMPLE=$3
+MD_BAM=${SAMPLE}.bam
+PE_BAM=${SAMPLE}___pe.bam
+RG_BAM=${SAMPLE}___rg.bam
+METRICS_FILE=${SAMPLE}___md.txt
+HS_METRICS_FILE=${SAMPLE}___hs.txt
+ALIGNMENT_SUMMARY_FILE=${SAMPLE}___AM.txt
+CMD_LOG=${SAMPLE}_commands.log
+# Load configuration file with references
+WORK_DIR=$(pwd)
+. $WORK_DIR/pipeline.config
+
+PICARD_CMD=/home/upops/Scripts/PicardScripts/picard
+BWA_CMD=/opt/common/CentOS_7/bwa/bwa-0.7.17/bwa
+
+RUN_AND_LOG() {
+   echo $NEXT_CMD >> $CMD_LOG
+   eval $NEXT_CMD
+}
+
+echo -e "Creating alignment bam \n\tO: ${PE_BAM} \n\tI: ${F1}, ${F2}"
+NEXT_CMD="$BWA_CMD mem -M -t 8 $REF_GENOME $F1 $F2  | samtools view -bS - > $PE_BAM"
+RUN_AND_LOG
+
+echo -e "Creating read-group bam \n\tO: ${RG_BAM} \n\tI: ${PE_BAM}" 
+NEXT_CMD="$PICARD_CMD AddOrReplaceReadGroups SO=coordinate CREATE_INDEX=true INPUT=$PE_BAM OUTPUT=$RG_BAM RGID=$SAMPLE RGLB=$SAMPLE RGPL=illumina RGPU=$SAMPLE RGSM=$SAMPLE RGCN=GCL@MSKCC"
+RUN_AND_LOG
+
+echo -e "Creating mark-dup bam \n\tO: ${MD_BAM} \n\tI: ${RG_BAM}"
+NEXT_CMD="$PICARD_CMD MarkDuplicates CREATE_INDEX=true METRICS_FILE=$METRICS_FILE OUTPUT=$MD_BAM INPUT=$RG_BAM"
+RUN_AND_LOG
+
+echo -e "Running hs metrics" 
+NEXT_CMD="$PICARD_CMD CollectHsMetrics \
+	BI=$BAIT_INTERVAL \
+	TI=$TARGET_INTERVAL \
+	I=$MD_BAM O=$HS_METRICS_FILE"
+RUN_AND_LOG
+
+echo -e "Running CollectAlignmentSummaryMetrics"
+NEXT_CMD="$PICARD_CMD CollectAlignmentSummaryMetrics \
+    MAX_INSERT_SIZE=1000 \
+    I=$MD_BAM \
+    O=$ALIGNMENT_SUMMARY_FILE \
+    R=$REF_GENOME"
+RUN_AND_LOG
+
+echo "DONE" 
---
+++ bsub_wrapper.sh
@@ -0,0 +1,33 @@
+if [[ -z "$1"  ||  -z "$2" || -z "$3" ]]
+  then
+    printf "Please provide a project sample & fastq files. See below,\n\t./bsub_wrapper.sh 07871_V /ifs/input/GCL/hiseq/FASTQ/PITT_0466_BHGKL7BBXY/Project_07871_V/Sample_P-0000386-N01-WES_IGO_07871_V_3/P-0000386-N01-WES_IGO_07871_V_3_S44_R2_001.fastq.gz /ifs/input/GCL/hiseq/FASTQ/PITT_0466_BHGKL7BBXY/Project_07871_V/Sample_P-0000386-N01-WES_IGO_07871_V_3/P-0000386-N01-WES_IGO_07871_V_3_S44_R1_001.fastq.gz\n"
+  exit 1
+fi
+
+SAMPLE=$1
+FASTQ1=$2
+FASTQ2=$3
+
+WORK_DIR=$(pwd)
+SCRIPT=$WORK_DIR/bam_creation.sh
+
+JOB_NAME="STAT_GEN:${SAMPLE}"
+
+# load config
+WORK_DIR=$(pwd)
+. $WORK_DIR/pipeline.config
+
+echo -e "Using following reference\n\tReference Genome: ${REF_GENOME}\n\tBait Set: ${BAIT_INTERVAL}\n\tTarget Set: ${TARGET_INTERVAL}"
+echo "Is this correct? Press 1/2"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) echo "Submitting job ${JOB_NAME}"; break;;
+        No ) echo "Please modify the pipeline.config file"; exit;;
+    esac
+done
+
+echo "Please provide an LSF node to run on"
+read LSF_NODE
+
+# We are running all of the jobs on a 72-CPU node
+bsub -J $JOB_NAME -o $JOB_NAME.out -m $LSF_NODE -n 72 -M 6 "$SCRIPT $FASTQ1 $FASTQ2 $SAMPLE"
