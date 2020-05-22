e53fc90da3d7d092ea2c8726b44cc13da3a0227c
DavidStreid <david.streid.nyc@gmail.com>
2020-05-22 13:31:43 -0400

    cleanup

--- contamination.sh
+++ contamination.sh
@@ -9,6 +9,7 @@ fi
 PROJECT_DIR=$1
 PROJECT=$2
 RESULTS=./contamination_output/${PROJECT}
+
 # load in commands
 . /home/streidd/pipeline-scripts/pipeline.config
 
@@ -19,12 +20,7 @@ if [ "$num_bams" -eq "0" ]; then
 fi
 
 echo "Running contamination on ${num_bams} BAMs. Results: ${RESULTS}"
-
 mkdir -p "${RESULTS}"
-
-
-# BAM=/ifs/res/IGO/fingerprint_reruns/06302_AH_5.21_rename/work/85/ba6b6251babd00409ca628fdcd499b/06302_AH__06302_AH_1472__C-YC8WEH__Tumor_headers.bam
-# BAM=/ifs/res/IGO/fingerprint_reruns/06302_AH_5.21_rename/work/ff/35da7565fe5d7f1753841c89438b46/06302_AH__06302_AH_1472__C-YC8WEH__Tumor_headers.bam
 
 for BAM in ${PROJECT_DIR}/*.bam;
 do
@@ -33,8 +29,6 @@ do
    contamination_result=${RESULTS}/contamination/${results_base}_contamination.table
 
    echo "Processing $results_base"
-
-   # exit
 
    $gatk GetPileupSummaries \
       -I ${BAM} \
