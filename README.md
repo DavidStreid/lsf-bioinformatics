d4cdbb9e31adcd890622e57def5f8154649f2416
DavidStreid <david.streid.nyc@gmail.com>
2020-08-24 11:26:38 -0400

    Updating demultiplex options & configurations

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Description
 Scripts that help coordinate jobs on an LSF computing cluster
+
+## Setup
+### `pipeline.config`
+Mandatory:
+`WORK_DIR=` - Set this to the absolute path of where the github project was checked out, e.g. `WORK_DIR=/home/streidd/work/lsf-bioinformatics`
+
+Probably Need:
+`bcl2fastq=`
+`gatk=`
--- demultiplex.sh
+++ demultiplex.sh
@@ -1,18 +1,20 @@
 # load config: need bcl2fastq
-. /home/streidd/pipeline-scripts/pipeline.config
+
 # bcl2fastq=/opt/common/CentOS_6/bcl2fastq/bcl2fastq2-v2.20.0.422/bin/bcl2fastq
 
-if [[ -z "$1"  ||  -z "$2" ]]
-   then
-      printf "please specify runpath & samplesheet file like - ./demultiplex.sh {RUN_PATH} {SAMPLESHEET}\ne.g.\n"
-      printf "\t./demultiplex.sh /ifs/input/GCL/hiseq/michelle/200317_MICHELLE_0220_AH3YNYDSXY /home/upops/nyx__SampleSheets/SampleSheet_200317_MICHELLE_0220_AH3YNYDSXY.csv\n"
-   exit 1
+if [[ -z "$1"  ||  -z "$2" || -z "$3" || -z "$4" ]]; then
+  printf "please specify runpath & samplesheet file like - ./demultiplex.sh {RUN_PATH} {SAMPLESHEET} {OUTPUT_DIR} {PIPELINE_CONFIG}\ne.g.\n"
+  printf "\t./demultiplex.sh /igo/sequencers/jax/200821_JAX_0461_AHHYJ5BBXY /home/igo/SampleSheetCopies/SampleSheet_200821_JAX_0461_AHHYJ5BBXY_A1.csv /igo/work/FASTQ/JAX_0461_AHHYJ5BBXY_A1\n"
+  exit 1
 fi
+
+PIPELINE_CONFIG=$4
+. $PIPELINE_CONFIG
 
 RUNPATH=$1
 SAMPLESHEET=$2
 RUN=$(basename $RUNPATH)
-OUTPUT=./${RUN}_bcl2fastq
+OUTPUT=$3
 
 printf "Demultiplexing $RUN\n\tRunPath:\t$RUNPATH\n\tSampleSheet:\t$SAMPLESHEET\n"
 
@@ -27,4 +29,6 @@ $bcl2fastq \
    --ignore-missing-positions \
    --ignore-missing-control \
    --barcode-mismatches 1 \
-   --no-lane-splitting 
+   --no-lane-splitting \
+   --loading-threads 12 \
+   --processing-threads 24
