f68d0272f8647643fc98a69adbe499a52d37959a
DavidStreid <david.streid.nyc@gmail.com>
2020-10-19 15:10:38 -0400

    Adding gc bias script

---
+++ bam_gc_bias.sh
@@ -0,0 +1,28 @@
+# !/bin/bash
+# Executes BWA_MEM w/ downsampling option
+# Run:
+#   ./bam_stats.sh -s gc_bias -b bam (-r REF_GENOME)
+
+while getopts b:r: flag
+do
+    case "${flag}" in
+        b) BAM=${OPTARG};;
+        r) REF_GENOME=${OPTARG};;
+    esac
+done
+
+# Check for mandatory arguments
+if [[ -z $BAM || -z $REF_GENOME ]]; then
+  echo "Usage: ./bam_stats.sh -b bam -r REF_GENOME"
+  exit 1
+fi
+
+# DERIVED ARGUMENTS
+SAMPLE=${BAM/.bam/}
+
+GC_BIAS_CMD="/home/igo/Scripts/PicardScripts/picard CollectGcBiasMetrics I=$BAM O=${SAMPLE}_gc_bias_metrics.txt CHART=${SAMPLE}_gc_bias_metrics.pdf S=${SAMPLE}_summary_metrics.txt R=${REF_GENOME}"
+JOB_NAME="GC_BIAS___${SAMPLE}"
+BSUB_CMD="bsub -J ${JOB_NAME} -o ${SAMPLE}.out -e ${SAMPLE}.err -n 36 -M 6 ${GC_BIAS_CMD}"
+echo "RUNNING JOB: ${JOB_NAME}"
+echo $BSUB_CMD
+$BSUB_CMD
