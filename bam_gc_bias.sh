a56ecf1f733af2570ebea7ed433fca5ac38a7cc7
DavidStreid <david.streid.nyc@gmail.com>
2020-10-20 16:41:48 -0400

    Allowing for absolute BAM path & general picard

--- bam_gc_bias.sh
+++ bam_gc_bias.sh
@@ -17,10 +17,17 @@ if [[ -z $BAM || -z $REF_GENOME ]]; then
   exit 1
 fi
 
-# DERIVED ARGUMENTS
-SAMPLE=${BAM/.bam/}
+if [ ! -f "$BAM" ]; then
+  echo "$BAM does not exist."
+  exit 1
+fi
 
-GC_BIAS_CMD="/home/igo/Scripts/PicardScripts/picard CollectGcBiasMetrics I=$BAM O=${SAMPLE}_gc_bias_metrics.txt CHART=${SAMPLE}_gc_bias_metrics.pdf S=${SAMPLE}_summary_metrics.txt R=${REF_GENOME}"
+BAM_FILE_NAME=$(basename $BAM)
+
+# DERIVED ARGUMENTS
+SAMPLE=${BAM_FILE_NAME/.bam/}
+
+GC_BIAS_CMD="picard CollectGcBiasMetrics I=$BAM O=${SAMPLE}_gc_bias_metrics.txt CHART=${SAMPLE}_gc_bias_metrics.pdf S=${SAMPLE}_summary_metrics.txt R=${REF_GENOME}"
 JOB_NAME="GC_BIAS___${SAMPLE}"
 BSUB_CMD="bsub -J ${JOB_NAME} -o ${SAMPLE}.out -e ${SAMPLE}.err -n 36 -M 6 ${GC_BIAS_CMD}"
 echo "RUNNING JOB: ${JOB_NAME}"
