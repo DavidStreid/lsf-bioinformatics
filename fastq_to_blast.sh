44dbb7e2b99245b4d4f4439af78a130fda6ac797
DavidStreid <david.streid.nyc@gmail.com>
2020-11-06 14:45:48 -0500

    Adding fastq_to_blast.sh

---
+++ fastq_to_blast.sh
@@ -0,0 +1,23 @@
+
+
+FASTQ=$1
+SAMPLE=$2
+
+if [[ ! -f $FASTQ ]]; then
+  echo "Need to specify FASTQ"
+  exit 1
+fi
+
+if [[ -z ${SAMPLE} ]]; then
+  SAMPLE="SAMPLE"
+fi
+
+JOB_NAME=FASTQ_${SAMPLE}
+OUTPUT_BAM="${SAMPLE}.bam"
+SUBMIT=$(bsub -J ${JOB_NAME} -e ${JOB_NAME}_error.log -o ${JOB_NAME}.log "java -jar /home/igo/resources/picard2.23.2/picard.jar FastqToSam O=${OUTPUT_BAM} F1=${FASTQ} SM=${SAMPLE} SO=coordinate && samtools index ${OUTPUT_BAM}")
+echo "SUBMIT=${SUBMIT}"
+JOB_ID=$(echo $SUBMIT | egrep -o '[0-9]{5,}')
+echo "Waiting for ${JOB_ID} to finish"
+
+JOB_NAME="BLAST_${SAMPLE}"
+bsub -w "done(${JOB_ID})" -J ${JOB_NAME} -e ${JOB_NAME}_error.log -o ${JOB_NAME}.log "/home/igo/bin/taxblast/MAIN_UNMAPPED_count.bsh ${OUTPUT_BAM}  nt 40 200 100"
